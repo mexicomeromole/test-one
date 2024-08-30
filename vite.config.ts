fc14ab6dad9ef24e5b8377db51cb4b14c82f7111
jorge <reytercero@gmail.com>
2024-08-29 19:37:59 -0600

    Resolviendo conflictos de merge

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/test-one/', // Asegúrate de que esta ruta sea correcta
-  build: {
-    outDir: 'dist',
-  },
+  base: '/test-one/', // Asegúrate de que esta ruta es correcta y corresponde al nombre de tu repositorio.
   plugins: [vue()],
 });
