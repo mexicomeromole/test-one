bcd626ec0d7018749d76e19ca8efa2fd97f4a246
jorge <reytercero@gmail.com>
2024-08-29 19:57:39 -0600

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+ 
 export default defineConfig({
   base: '/test-one/', // Esto asegura que las rutas de los recursos coincidan con la subcarpeta en GitHub Pages
   build: {
