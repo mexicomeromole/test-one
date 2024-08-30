7bc7b5e14405371967180c336f44597727dfb9b6
jorge <reytercero@gmail.com>
2024-08-29 22:31:49 -0600

    Deploy build from dist to main branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '/test-one/', // Esto asegura que las rutas de los recursos coincidan con la subcarpeta en GitHub Pages
   build: {
-    outDir: 'dist',  // Carpeta de salida para la compilación
+    outDir: 'docs',  // Carpeta de salida para la compilación
   },
   plugins: [vue()],
 });
